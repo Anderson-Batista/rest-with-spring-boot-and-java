757aae55c6a61b4d524eb193991418b309b5d076
Anderson-Batista <andersonbfs15@gmail.com>
2024-08-15 15:29:34 -0300

    requisicoes HTTP

---
+++ rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/anderson/controllers/PersonController.java
@@ -0,0 +1,44 @@
+package br.com.anderson.controllers;
+
+import br.com.anderson.model.Person;
+import br.com.anderson.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+    //private PersonServices services = new PersonServices();
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") String id) {
+        return service.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @RequestMapping(value = "/{id}")
+    public void delete(@PathVariable(value = "id") String id) {
+        service.delete(id);
+    }
+
+}
