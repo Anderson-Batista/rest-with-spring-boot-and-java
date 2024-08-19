92b7965d03ff7d70e97e2fea1a06ee103f72cd63
Anderson-Batista <andersonbfs15@gmail.com>
2024-08-19 16:29:03 -0300

    adicionando padrao VO semlehante ao DTO e mappers

--- rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/anderson/services/PersonServices.java
+++ rest-with-spring-boot-and-java/rest-with-spring-boot-and-java/src/main/java/br/com/anderson/services/PersonServices.java
@@ -1,14 +1,13 @@
 package br.com.anderson.services;
 
+import br.com.anderson.data.vo.v1.PersonVO;
 import br.com.anderson.exceptions.ResourceNotFoundException;
 import br.com.anderson.model.Person;
 import br.com.anderson.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 @Service
@@ -18,26 +17,26 @@ public class PersonServices {
     @Autowired
     PersonRepository repository;
 
-    public List<Person> findAll() {
+    public List<PersonVO> findAll() {
         logger.info("Find all people!");
 
         return repository.findAll();
     }
 
-    public Person findById(Long id) {
+    public PersonVO findById(Long id) {
         logger.info("Find one person!");
 
         return repository.findById(id).orElseThrow(() ->new ResourceNotFoundException("No records found for this ID!"));
     }
 
-    public Person create(Person person) {
+    public PersonVO create(PersonVO person) {
 
         logger.info("Creating one person!");
 
         return repository.save(person);
     }
 
-    public Person update(Person person) {
+    public PersonVO update(PersonVO person) {
 
         logger.info("Updating one person!");
 
